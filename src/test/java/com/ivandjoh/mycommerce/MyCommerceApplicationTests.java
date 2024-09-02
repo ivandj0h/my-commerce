b294ea646aa48027fcfd24d4582e5f9dfa571e7c
ivandjoh <ivandi.djoh@gmail.com>
2024-09-02 13:36:25 +0800

    init

---
+++ src/test/java/com/ivandjoh/mycommerce/MyCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivandjoh.mycommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
